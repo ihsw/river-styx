ee4a75d71fe6074606f9ba4625f536c18b2bf5b2
Adrian Parker <adrian.parker@smith.co>
2016-08-31 12:13:10 -0400

    Finish switching over to ava and async tests.

--- tests/ChildProcess.ts
+++ tests/ChildProcess.ts
@@ -5,85 +5,62 @@ import { ChildProcess } from "../src/ChildProcess";
 test("ChildProcess runs", async (t) => {
     let childProcess = new ChildProcess(`${__dirname}/../tests-fixtures/run`);
     childProcess.run();
+
     childProcess = await childProcess.onExit();
     t.is(childProcess.exitCode, 0, "Exits with 0");
 });
 
-// test("ChildProcess fails with 1", (t) => {
-//     const childProcess = new ChildProcess(`${__dirname}/../tests-fixtures/run-fail`);
-//     childProcess.run();
-//     childProcess.onExit().then((childProcess: ChildProcess) => {
-//         t.is(childProcess.exitCode, 1, "Exits with 1");
-//         t.end();
-//     }).catch((err: Error) => {
-//         t.is(err, null);
-//         t.end();
-//     });
-// });
+test("ChildProcess fails with 1", async (t) => {
+    let childProcess = new ChildProcess(`${__dirname}/../tests-fixtures/run-fail`);
+    childProcess.run();
 
-// test("ChildProcess disconnects", (t) => {
-//     const childProcess = new ChildProcess(`${__dirname}/../tests-fixtures/disconnect`);
-//     childProcess.run();
-//     childProcess.disconnect();
-//     childProcess.onExit().then((childProcess: ChildProcess) => {
-//         t.is(childProcess.exitCode, 0, "Exits with 0");
-//         t.end();
-//     }).catch((err: Error) => {
-//         t.is(err, null);
-//         t.end();
-//     });
-// });
+    childProcess = await childProcess.onExit();
+    t.is(childProcess.exitCode, 1, "Exits with 1");
+});
 
-// test("ChildProcess disconnect with timeout", (t) => {
-//     const startTime = process.hrtime();
-//     const childProcess = new ChildProcess(`${__dirname}/../tests-fixtures/disconnect-with-timeout`);
-//     childProcess.run();
+test("ChildProcess disconnects", async (t) => {
+    let childProcess = new ChildProcess(`${__dirname}/../tests-fixtures/disconnect`);
+    childProcess.run();
 
-//     const expectedDuration = 2.5 * 1000;
-//     childProcess.disconnectWithTimeout(expectedDuration, "SIGINT").then((childProcess) => {
-//         const [durationSeconds, durationMs] = process.hrtime(startTime);
-//         const actualDuration = durationSeconds + (durationMs / (1 * 1000 * 1000 * 1000));
+    childProcess.disconnect();
+    childProcess = await childProcess.onExit();
+    t.is(childProcess.exitCode, 0, "Exits with 0");
+});
 
-//         t.is(Number(actualDuration.toFixed(1)), expectedDuration / 1000, "Timeout is 2.5s");
-//         t.is(childProcess.exitCode, null, "Null exit code");
-//         t.is(childProcess.exitSignal, "SIGINT", "SIGINT exit signal");
-//         t.end();
-//     }).catch((err: Error) => {
-//         t.is(err, null);
-//         t.end();
-//     });
-// });
+test("ChildProcess disconnect with timeout", async (t) => {
+    const startTime = process.hrtime();
+    let childProcess = new ChildProcess(`${__dirname}/../tests-fixtures/disconnect-with-timeout`);
+    childProcess.run();
 
-// test("ChildProcess is killed", (t) => {
-//     const childProcess = new ChildProcess(`${__dirname}/../tests-fixtures/run`);
-//     childProcess.run();
-//     childProcess.kill("SIGINT");
-//     childProcess.onExit().then((childProcess: ChildProcess) => {
-//         t.is(childProcess.exitCode, null, "Null exit code");
-//         t.is(childProcess.exitSignal, "SIGINT", "SIGINT exit signal");
-//         t.end();
-//     }).catch((err: Error) => {
-//         t.is(err, null);
-//         t.end();
-//     });
-// });
+    const expectedDuration = 2.5 * 1000;
+    childProcess = await childProcess.disconnectWithTimeout(expectedDuration, "SIGINT");
+    const [durationSeconds, durationMs] = process.hrtime(startTime);
+    const actualDuration = durationSeconds + (durationMs / (1 * 1000 * 1000 * 1000));
+    t.is(Number(actualDuration.toFixed(1)), expectedDuration / 1000, "Timeout is 2.5s");
+    t.is(childProcess.exitCode, null, "Null exit code");
+    t.is(childProcess.exitSignal, "SIGINT", "SIGINT exit signal");
+});
 
-// test("ChildProcess receives and returns a message", (t) => {
-//     const childProcess = new ChildProcess(`${__dirname}/../tests-fixtures/receives-message`);
-//     childProcess.run();
+test("ChildProcess is killed", async (t) => {
+    let childProcess = new ChildProcess(`${__dirname}/../tests-fixtures/run`);
+    childProcess.run();
 
-//     const greeting = "Hello, world!";
-//     childProcess.send(greeting);
-//     childProcess.receive().then((response: any) => {
-//         t.is(response, greeting, "Response matches message sent");
-//         childProcess.disconnect();
+    childProcess.kill("SIGINT");
+    childProcess = await childProcess.onExit();
+    t.is(childProcess.exitCode, null, "Null exit code");
+    t.is(childProcess.exitSignal, "SIGINT", "SIGINT exit signal");
+});
 
-//         return childProcess.onExit();
-//     }).then((childProcess: ChildProcess) => {
-//         t.is(childProcess.exitCode, 0, "Exits with 0");
-//         t.end();
-//     }).catch((err: Error) => {
-//         t.is(err, null);
-//         t.end();
-//     });
-// });
+test("ChildProcess receives and returns a message", async (t) => {
+    let childProcess = new ChildProcess(`${__dirname}/../tests-fixtures/receives-message`);
+    childProcess.run();
+
+    const greeting = "Hello, world!";
+    childProcess.send(greeting);
+    const response = await childProcess.receive();
+    t.is(response, greeting, "Response matches message sent");
+    childProcess.disconnect();
+
+    childProcess = await childProcess.onExit();
+    t.is(childProcess.exitCode, 0, "Exits with 0");
+});
