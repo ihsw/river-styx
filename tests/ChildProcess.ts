002a340c47100199967a4033f6bf74a35870f277
Adrian Parker <adrian.parker@smith.co>
2016-08-31 12:20:20 -0400

    Add main.d.ts for incorporating non-typings.

--- tests/ChildProcess.ts
+++ tests/ChildProcess.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/index.d.ts" />
+/// <reference path="../typings/main.d.ts" />
 import { test } from "ava";
 import { ChildProcess } from "../src/ChildProcess";
 
---
+++ typings/main.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="./index.d.ts" />
+/// <reference path="../node_modules/ava/types/generated.d.ts" />
