514e0ab76b9f3f20ca09c33ed09faeeb1f815bce
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-09-01 17:53:45 -0400

    Start load-route and unload-route functionality.

--- src/WebFrontend.ts
+++ src/WebFrontend.ts
@@ -1,10 +1,40 @@
 /// <reference path="../typings/index.d.ts" />
 import * as express from "express";
+import * as supertest from "supertest";
+import { ChildProcess } from "./ChildProcess";
+
+interface IRoutes {
+    [routePrefix: string]: ChildProcess;
+}
 
 export class WebFrontend {
     app: express.Application;
+    routes: IRoutes;
 
     constructor() {
         this.app = express();
+        this.routes = {};
+    }
+
+    loadRoute(routePrefix: string, childProcess: ChildProcess) {
+        this.routes[routePrefix] = childProcess;
+
+        childProcess.run();
+        this.app.all(routePrefix, (req, res) => {
+            childProcess.send(req);
+            childProcess.receive().then((message: supertest.Response) => {
+                res.send(message.text);
+            });
+        });
+    }
+
+    unloadRoute(routePrefix: string): Promise<ChildProcess> {
+        if (!(routePrefix in this.routes)) {
+            throw new Error(`Route ${routePrefix} could not be found!`);
+        }
+
+        const route = this.routes[routePrefix];
+        route.disconnect();
+        return route.onExit();
     }
 }
--- tests/WebFrontend.ts
+++ tests/WebFrontend.ts
@@ -3,13 +3,28 @@ import { test } from "ava";
 import * as supertest from "supertest";
 import * as HttpStatus from "http-status";
 import { WebFrontend } from "../src/WebFrontend";
+import { ChildProcess } from "../src/ChildProcess";
 
 test.cb("Blank WebFrontend should return NOT_FOUND", (t) => {
     const webFrontend = new WebFrontend();
     const request = supertest(webFrontend.app);
     request.get("/")
         .end((err: Error, res: supertest.Response) => {
-            t.is(res.status, HttpStatus.NOT_FOUND);
+            t.is(res.status, HttpStatus.NOT_FOUND, "Response code is NOT_FOUND");
+            t.end();
+        });
+});
+
+test.cb.only("WebFrontend should load basic route", (t) => {
+    const webFrontend = new WebFrontend();
+
+    const childProcess = new ChildProcess("../tests-fixtures/express-response");
+    webFrontend.loadRoute("/greeting", childProcess);
+
+    const request = supertest(webFrontend.app);
+    request.get("/greeting")
+        .end((err: Error, res: supertest.Response) => {
+            t.is(res.text, "Hello, world!", "Response text is standard greeting");
             t.end();
         });
 });
